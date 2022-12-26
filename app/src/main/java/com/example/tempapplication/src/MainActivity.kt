c4adb2eb75b78e0733774e6f38003a5f069f6742
YuJungHyun <0703olivia@naver.com>
2022-12-27 00:29:30 +0900

    ~14번

--- app/src/main/java/com/example/tempapplication/src/MainActivity.kt
+++ app/src/main/java/com/example/tempapplication/src/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tempapplication
+package com.example.tempapplication.src
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tempapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/tempapplication/src/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tempapplication.src.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.tempapplication.config.BaseActivity
+import com.example.tempapplication.databinding.ActivitySplashBinding
+import com.example.tempapplication.src.MainActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+
+    }
+}
